a3cacfcbeac2376b0dbf5d27ebc03e2ff10550b3
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2023-11-17 15:18:26 -0300

    criado arquivos de rotas

---
+++ src/routes/equipes.routes.js
@@ -0,0 +1,14 @@
+const { Router } = require("express")
+
+const NotesController = require("../controllers/NotesController")
+
+const notesController = new NotesController()
+
+const notesRoutes = Router()
+
+notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.get("/:id", notesController.show)
+notesRoutes.delete("/:id", notesController.delete)
+notesRoutes.get("/", notesController.index)
+
+module.exports = notesRoutes;
---
+++ src/routes/index.js
@@ -0,0 +1,30 @@
+//importa o Router do express
+const { Router } = require("express");
+
+//rota para a rota do usuário
+const  authRouter  = require("./autenticacao.routes");
+const  colaboradoresRouter  = require("./colaboradores.routes");
+const  equipesRouter = require("./equipes.routes")
+const  obrasRouter = require("./obras.routes")
+const  obras_turnosRouter = require("./obras_turnos.routes")
+const  programacoesRouter = require("./programacoes.routes")
+const  servicosRouter = require("./servicos.routes")
+const  turnosRouter = require("./turnos.routes")
+const  veiculosRouter = require("./veiculos.routes")
+
+//insere a constante em um "app", nesse caso routes
+const routes = Router();
+
+//fala pra o aplicativo usar o arquivo users.routes.js pra acessar os métodos
+routes.use("/auth", authRouter);
+routes.use("/colaboradores", colaboradoresRouter);
+routes.use("/equipes", equipesRouter);
+routes.use("/obras", obrasRouter);
+routes.use("/obras_turnos", obras_turnosRouter);
+routes.use("/programacoes", programacoesRouter);
+routes.use("/servicos", servicosRouter);
+routes.use("/turnos", turnosRouter);
+routes.use("/veiculos", veiculosRouter);
+
+// exporta as rotas
+module.exports = routes;
