39f03b6ede8790bc4d655fc53c4c0438b3eebb84
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2023-11-17 11:50:17 -0300

    só uma data tabela programações

--- src/database/knex/migrations/20231116171519_programacoes.js
+++ src/database/knex/migrations/20231116171519_programacoes.js
@@ -3,8 +3,7 @@ exports.up = (knex) =>
     table.bigIncrements("id");
     table.bigInteger("obra_id").references("id").inTable("obras").notNullable();
     table.bigInteger("equipe_id").references("id").inTable("equipes").notNullable();
-    table.date("data_inicial").notNullable();
-    table.date("data_final").notNullable();
+    table.date("data").notNullable();
   });
 
 exports.down = (knex) => knex.schema.dropTable("programacoes");
