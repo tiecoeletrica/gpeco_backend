5b0367cbdd6a8c86de0afad8e936cdfedaed100f
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2023-11-17 15:45:57 -0300

    controllers criados

---
+++ src/controllers/ProgramacoesController.js
@@ -0,0 +1,33 @@
+const knex = require("../database/knex")
+const AppError = require("../utils/AppError") //importa biblioteca de erros
+
+
+
+class ProgramacoesController {
+    async create(request, response) {
+        
+
+        response.status(201).json();
+    }
+
+    async show(request, response) {
+        
+        return response.status(200).json({
+           
+        })
+    }
+
+    async delete(request, response) {
+       
+        return response.status(201).json("Nota deletada")
+    }
+
+    async index(request, response) {
+        
+        
+        response.status(200).json(notesWhithTags)
+    }
+}
+
+
+module.exports = ProgramacoesController
