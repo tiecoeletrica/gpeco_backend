2f185579c4d7d3186a24380d522b699ff6c46a15
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2023-11-17 09:19:09 -0300

    novas colunas colaboradores

--- src/database/knex/migrations/20231116170628_colaboradores.js
+++ src/database/knex/migrations/20231116170628_colaboradores.js
@@ -3,8 +3,11 @@ exports.up = (knex) =>
     table.bigIncrements("id");
     table.string("nome").notNullable();
     table.bigInteger("cpf").notNullable();
+    table.bigInteger("email").notNullable();
+    table.bigInteger("senha").notNullable();
     table.bigInteger("equipe_id").references("id").inTable("equipes");
     table.string("tipo").notNullable();
+    table.boolean("status").notNullable().default(1);
   });
 
 exports.down = (knex) => knex.schema.dropTable("colaboradores");
