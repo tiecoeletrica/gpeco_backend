cd3ea9ca75209a0fb80b0426fbce2791205ed772
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2023-11-27 16:11:24 -0300

    programacao create

--- src/controllers/ProgramacoesController.js
+++ src/controllers/ProgramacoesController.js
@@ -1,67 +1,68 @@
-const knex = require("../database/knex")
-const AppError = require("../utils/AppError") //importa biblioteca de erros
-
-
+const knex = require("../database/knex");
+const AppError = require("../utils/AppError"); //importa biblioteca de erros
 
 class ProgramacoesController {
-    async create(request, response) {
-        const programacoes = request.body;
+  async create(request, response) {
+    const programacoes = request.body;
 
-        if (!Array.isArray(programacoes)) {
-            throw new AppError("O corpo da solicitação deve conter um array de programacoes");
-        }
-
-
-        var repetidos = await knex("orcamentos")
-            .select("N_PROJETO", "MATERIAL", "ORCADO")
-            .whereIn(
-                ["N_PROJETO", "MATERIAL", "ORCADO"],
-                programacoes.map(material => [material.N_PROJETO, material.MATERIAL, material.ORCADO]))
-            .distinct()
-
-
-        if (repetidos.length > 0) {
-            repetidos = repetidos.map(repetido => repetido.N_PROJETO + " - " + repetido.MATERIAL + " - " + repetido.ORCADO)
-            throw new AppError(`Já existem esses orçamentos: ${repetidos}`)
-        }
-
-
-        programacoes.map(async (material) => {
-            const { N_PROJETO, MATERIAL, ORCADO } = material
-
-            await knex("orcamentos").insert({
-                N_PROJETO, MATERIAL, ORCADO
-            })
-        })
-
-
-        response.status(201).json();
+    if (!Array.isArray(programacoes)) {
+      throw new AppError(
+        "O corpo da solicitação deve conter um array de programações"
+      );
     }
 
-    async show(request, response) {
-        
-        return response.status(200).json({
-           
-        })
-    }
+    // console.log(programacoes)
+    var programacao = [];
+    programacao = programacoes.map(async (prog) => {
+      let { obra, equipe, data } = prog;
+      const obraID = await knex("obras")
+        .select(["id"])
+        .where({ projeto: obra })
+        .first();
 
-    async delete(request, response) {
-       
-        return response.status(201).json("Nota deletada")
-    }
+      const equipeID = await knex("equipes")
+        .select(["id"])
+        .where({ equipe: equipe })
+        .first();
 
-    async index(request, response) {
-        
-        
-        response.status(200).json()
-    }
+      var auxData = data.split("/");
+      auxData = auxData[2] + "-" + auxData[1] + "-" + auxData[0];
 
-    async update(request, response) {
-        
-        
-        response.status(200).json()
-    }
+      //console.log(obraID.id, equipeID.id, auxData);
+      return { obra_id: obraID.id, equipe_id: equipeID.id, data: auxData };
+    });
+
+    const resolvedProgramacao = await Promise.all(programacao);
+
+    await knex("programacoes").delete()
+    resolvedProgramacao.map(async (prog) => {
+      const { obra_id, equipe_id, data } = prog;
+
+      await knex("programacoes").insert({
+        obra_id,
+        equipe_id,
+        data
+      });
+    });
+
+    response.status(201).json("Programação inserida com sucesso");
+  }
+
+  async show(request, response) {
+    return response.status(200).json({});
+  }
+
+  async delete(request, response) {
+    return response.status(201).json("Nota deletada");
+  }
+
+  async index(request, response) {
+    response.status(200).json();
+  }
+
+  async update(request, response) {
+    response.status(200).json();
+  }
 }
 
-
-module.exports = ProgramacoesController
+module.exports = ProgramacoesController;
