fd31d35384df0e07841428a87cd5be56552028a0
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2023-11-27 09:37:57 -0300

    obras create

--- src/controllers/ObrasController.js
+++ src/controllers/ObrasController.js
@@ -5,9 +5,36 @@ const AppError = require("../utils/AppError") //importa biblioteca de erros
 
 class ObrasController {
     async create(request, response) {
-        
+        const obras = request.body;
 
-        response.status(201).json();
+        if (!Array.isArray(obras)) {
+            throw new AppError("O corpo da solicitação deve conter um array de obras");
+        }
+
+
+        var repetidos = await knex("obras")
+            .select("projeto", "carteira")
+            .whereIn(
+                ["projeto", "carteira"],
+                obras.map(obra => [obra.projeto, obra.carteira]))
+            .distinct()
+
+
+        if (repetidos.length > 0) {
+            repetidos = repetidos.map(repetido => repetido.projeto + " - " + repetido.carteira)
+            throw new AppError(`Essas obras já estão no banco de dados: ${repetidos}`)
+        }
+
+
+        obras.map(async (obra) => {
+            const { projeto, descricao, status, carteira, cidade, utd } = obra
+
+            await knex("obras").insert({
+                projeto, descricao, status, carteira, cidade, utd 
+            })
+        })
+
+        response.status(201).json("Obras inseridas no GPECO");
     }
 
     async show(request, response) {
--- src/controllers/ProgramacoesController.js
+++ src/controllers/ProgramacoesController.js
@@ -5,7 +5,35 @@ const AppError = require("../utils/AppError") //importa biblioteca de erros
 
 class ProgramacoesController {
     async create(request, response) {
-        
+        const programacoes = request.body;
+
+        if (!Array.isArray(programacoes)) {
+            throw new AppError("O corpo da solicitação deve conter um array de programacoes");
+        }
+
+
+        var repetidos = await knex("orcamentos")
+            .select("N_PROJETO", "MATERIAL", "ORCADO")
+            .whereIn(
+                ["N_PROJETO", "MATERIAL", "ORCADO"],
+                programacoes.map(material => [material.N_PROJETO, material.MATERIAL, material.ORCADO]))
+            .distinct()
+
+
+        if (repetidos.length > 0) {
+            repetidos = repetidos.map(repetido => repetido.N_PROJETO + " - " + repetido.MATERIAL + " - " + repetido.ORCADO)
+            throw new AppError(`Já existem esses orçamentos: ${repetidos}`)
+        }
+
+
+        programacoes.map(async (material) => {
+            const { N_PROJETO, MATERIAL, ORCADO } = material
+
+            await knex("orcamentos").insert({
+                N_PROJETO, MATERIAL, ORCADO
+            })
+        })
+
 
         response.status(201).json();
     }
