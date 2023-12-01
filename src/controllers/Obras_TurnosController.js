f989019618444053e3dd06af1cd536de09e84427
Rodrigo Frutuoso <rodfrutuoso@hotmail.com>
2023-12-01 15:16:31 -0300

    obras_turnos lancamentos ajustado

--- src/controllers/Obras_TurnosController.js
+++ src/controllers/Obras_TurnosController.js
@@ -3,7 +3,7 @@ const AppError = require("../utils/AppError"); //importa biblioteca de erros
 
 class Obras_TurnosController {
   async create(request, response) {
-    const { obra_id, turno_id, fase_da_obra, retorno_campo, fora_programacao } = request.body;
+    const { obra_id, turno_id, fase_da_obra, retorno_campo, fora_programacao, lancamentos } = request.body;
 
     const [testeTurno] = await knex("turnos").where({ id: turno_id });
 
@@ -12,12 +12,11 @@ class Obras_TurnosController {
     }
 
     const [testeObra] = await knex("obras").where({ id: obra_id });
-    console.log(testeObra)
     if (!testeObra) {
       throw new AppError("Essa obra não está cadastrada");
     }
 
-    await knex("obras_turnos").insert({
+    var obras_turnosID = await knex("obras_turnos").insert({
       obra_id,
       turno_id,
       fase_da_obra,
@@ -25,15 +24,25 @@ class Obras_TurnosController {
       fora_programacao,
     });
 
+    obras_turnosID = await Promise.all(obras_turnosID)
+
+    lancamentos.map(async lanc => {
+      await knex("lancamentos").insert({
+        obras_turnos_id: obras_turnosID[0],
+        servico_id: lanc.servico_id,
+        quantidade: lanc.quantidade
+      })
+    })
+
     response.status(201).json("Serviço de obra lançado com sucesso");
   }
 
   async show(request, response) {
-    const {id} = request.params
+    const { id } = request.params
 
-    const [obra] = await knex("obras").where({id})
+    const [obra] = await knex("obras").where({ id })
 
-    if(!obra) throw new AppError("Obra não encontrada")
+    if (!obra) throw new AppError("Obra não encontrada")
 
     return response.status(200).json(obra);
 
@@ -44,14 +53,14 @@ class Obras_TurnosController {
   }
 
   async index(request, response) {
-    const {turno_id} = request.query
+    const { turno_id } = request.query
 
     let filter = {};
 
     if (turno_id) filter.turno_id = turno_id;
 
     const equipes = await knex("obras_turnos")
-        .where(filter)
+      .where(filter)
 
     response.status(200).json(equipes);
   }
